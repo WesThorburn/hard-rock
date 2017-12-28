0170b0f8cd2b01e601a975966ac1c26b24c6be6c
Wes <wjthorburn@live.com.au>
2017-12-28 16:06:58 -0600

    Print total cargo value on hud

--- payload.h
+++ payload.h
@@ -84,16 +84,21 @@ struct Payload{
 	}
 
 	void print(){
-		if(this->copper_qty){ fillText(0, "Copper: " + std::to_string(this->copper_qty), 3, 82);}
-		if(this->iron_qty){ fillText(0, "Iron: " + std::to_string(this->iron_qty), 3, 94);}
-		if(this->coal_qty){ fillText(0, "Coal: " + std::to_string(this->coal_qty), 3, 106);}
-		if(this->silver_qty){ fillText(0, "Silver: " + std::to_string(this->silver_qty), 3, 118);}
-		if(this->gold_qty){ fillText(0, "Gold: " + std::to_string(this->gold_qty), 3, 130);}
-		if(this->platinum_qty){ fillText(0, "Platinum: " + std::to_string(this->platinum_qty), 3, 142);}
-		if(this->sapphire_qty){ fillText(0, "Sapphire: " + std::to_string(this->sapphire_qty), 3, 154);}
-		if(this->emerald_qty){ fillText(0, "Emerald: " + std::to_string(this->emerald_qty), 3, 166);}
-		if(this->ruby_qty){ fillText(0, "Ruby: " + std::to_string(this->ruby_qty), 3, 178);}
-		if(this->diamond_qty){ fillText(0, "Diamond: " + std::to_string(this->diamond_qty), 3, 190);}
+		int startingYPos = 82;
+		int totalPayloadValue = getTotalValue();
+		if(this->copper_qty){ fillText(0, "Copper: " + std::to_string(this->copper_qty), 3, startingYPos += 12);}
+		if(this->iron_qty){ fillText(0, "Iron: " + std::to_string(this->iron_qty), 3, startingYPos += 12);}
+		if(this->coal_qty){ fillText(0, "Coal: " + std::to_string(this->coal_qty), 3, startingYPos += 12);}
+		if(this->silver_qty){ fillText(0, "Silver: " + std::to_string(this->silver_qty), 3, startingYPos += 12);}
+		if(this->gold_qty){ fillText(0, "Gold: " + std::to_string(this->gold_qty), 3, startingYPos += 12);}
+		if(this->platinum_qty){ fillText(0, "Platinum: " + std::to_string(this->platinum_qty), 3, startingYPos += 12);}
+		if(this->sapphire_qty){ fillText(0, "Sapphire: " + std::to_string(this->sapphire_qty), 3, startingYPos += 12);}
+		if(this->emerald_qty){ fillText(0, "Emerald: " + std::to_string(this->emerald_qty), 3, startingYPos += 12);}
+		if(this->ruby_qty){ fillText(0, "Ruby: " + std::to_string(this->ruby_qty), 3, startingYPos += 12);}
+		if(this->diamond_qty){ fillText(0, "Diamond: " + std::to_string(this->diamond_qty), 3, startingYPos += 12);}
+		setFontSize(0, 14);
+		setFillStyle(0, 0, 0, 0);
+		if(totalPayloadValue){ fillText(0, "Cargo Value: $" + std::to_string(totalPayloadValue) + ".00", 3, startingYPos += 14);}
 	}
 
 	void reset(){
